ceb57621d6595597d0fef68583a2379acb4525c9
Maik Klein <maikklein@googlemail.com>
2013-08-08 12:15:37 +0200

    update1

--- examples/demo1.rs
+++ examples/demo1.rs
@@ -5,19 +5,23 @@ pub fn main() {
         do new_game(~"my game") {
 
             let lvl = Level::new(800,600);
-            let mut count = 0i16;
-            let mut dt = DeltaTime::new();
-            let mut deltasum = 0.0f;
-            let mut actor = Actor::new(0,0,50,50);
+            
+           
+            let actor1 = GameActor::new(0,0,50,50,
+                                        200,100,20);
+    
+            let mut actors = ~[actor1];
 
             do default_game_loop(&lvl){
-                if deltasum > 0.0001f {
-                    deltasum = 0.0f;
-                    count +=1;
-            }
 
-            deltasum += dt.get_dt();
-            actor.update(count,count).redraw(&lvl);
-            }
+                for a in actors.mut_iter() {
+                    if a.rect.x as int > lvl.width {
+                        a.update_pos(-a.rect.x,0).redraw(&lvl);
+                    }
+                    else {
+                        a.move_right().redraw(&lvl);
+                    }
+                }  
+            } 
         }
     }
--- src/game.rs
+++ src/game.rs
@@ -1,23 +1,34 @@
 extern mod sdl;
 extern mod extra;
-
-pub struct Actor{
-    rect: sdl::Rect
+pub struct Color{
+    r: u8,
+    g: u8,
+    b: u8
+}
+pub struct GameActor{
+    rect: sdl::Rect,
+    speed: i16,
+    color: Color
 }
 pub struct Level{
     width: int,
     height: int,
     block_size: int,
-    actors: ~[Actor],
     screen: sdl::video::Surface
 }
 pub struct DeltaTime{
     current_time: float,
     old_time: float
 }
+trait Move{
+    fn move_up<'r>(&'r mut self)-> &'r mut Self;
+    fn move_down<'r>(&'r mut self)-> &'r mut Self;
+    fn move_left<'r>(&'r mut self)-> &'r mut Self;
+    fn move_right<'r>(&'r mut self)-> &'r mut Self;
+    
+}
 impl DeltaTime {
     pub fn new() -> DeltaTime {
-        
         DeltaTime {current_time: 0.0f, old_time: 0.0f}   
     }
     pub fn get_dt(&mut self) -> float{
@@ -35,29 +46,51 @@ impl Level {
             Ok(screen) => screen,
             Err(err) => fail!(fmt!("failed to set video mode: %s", err))
         };
-        Level{width: width, height: height,block_size: 1, actors: ~[], screen: *screen}
+        Level{width: width, height: height,block_size: 1, screen: *screen}
         
     }
 }
 
-impl Actor {
-    pub fn update <'a>(&'a mut self,x: i16,y:i16) -> &'a mut Actor {
-        self.rect.x = x;
-        self.rect.y = y; 
+impl GameActor {
+    pub fn update_pos <'a>(&'a mut self,x: i16,y:i16) -> &'a mut GameActor {
+        self.rect.x += x;
+        self.rect.y += y; 
         self
     }
     pub fn redraw(&self, lvl: &Level) {
-        lvl.screen.fill_rect(Some(self.rect), sdl::video::RGBA(0,0,255,255));
+        lvl.screen.fill_rect(Some(self.rect), sdl::video::RGBA(self.color.r,self.color.g,self.color.b,255));
     }
-    pub fn new(x: i16,y: i16, width: u16, height: u16) -> Actor{
-        Actor {rect: sdl::Rect {
+    pub fn new(x: i16,y: i16, width: u16, height: u16, r:u8,g:u8,b:u8) -> GameActor{
+        GameActor {rect: sdl::Rect {
                     x: x,
                     y: y,
                     w: width,
-                    h: height}}
+                    h: height},
+                   speed: 5,
+                   color: Color{r:r,g:g,b:b}
+                   
+        }
     }
 }
-
+impl Move for GameActor {
+    fn move_up<'r>(&'r mut self)-> &'r mut GameActor {
+        self.update_pos(0,self.speed);
+        self
+    }
+    fn move_down<'r>(&'r mut self)-> &'r mut GameActor {
+        self.update_pos(0,-self.speed);
+        self
+    }
+    fn move_left<'r>(&'r mut self)-> &'r mut GameActor {
+        self.update_pos(-self.speed,0);
+        self
+    }
+    fn move_right<'r>(&'r mut self)-> &'r mut GameActor {
+        self.update_pos(self.speed,0);
+        self
+    }
+    
+}
 pub fn new_game(name: ~str,f: ~fn()) {
     do sdl::start {
         sdl::init([sdl::InitVideo]);
@@ -67,15 +100,22 @@ pub fn new_game(name: ~str,f: ~fn()) {
     }
 }
 pub fn default_game_loop(lvl: &Level, f: &fn() ){
+    let mut dt = DeltaTime::new();
+    let mut dt_sum = 0.0f;
     loop{
-    lvl.screen.fill(sdl::video::RGBA(0,0,0,255));
-    f();
-    match sdl::event::poll_event() {
+        match sdl::event::poll_event() {
                 sdl::event::QuitEvent => break,
                 _ => {}
-    }
-            
-    lvl.screen.flip(); 
+        }
+        if dt_sum > 0.00001f {
+        lvl.screen.fill(sdl::video::RGBA(0,0,0,255));
+        f();    
+        lvl.screen.flip();
+        dt_sum = 0.0f 
+        }
+        else {
+            dt_sum += dt.get_dt();
+        }
     }
 
 }
